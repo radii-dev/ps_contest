c35d2ca0a6f4ab53d205bfa562f942c614277f80
Seongwook Kang <gigaslender2@gachon.ac.kr>
2021-07-25 20:02:47 +0900

    Seperate readInput() method

--- SCPC/practice/2020pre1/p2/p2.cpp
+++ SCPC/practice/2020pre1/p2/p2.cpp
@@ -5,26 +5,25 @@ using namespace std;
 
 int Answer;
 int n, k;
-int AwinCase, BwinCase;
+int X[3001], Y[3001];
+int dp[3001][3001];
 
-char checkWhoWin(char turn, vector<int> leftX, vector<int> leftY) {
-	int sumx, sumy;
-	for (int i = 0; i < leftX.size(); i++) {
-		sumx += leftX.at(i);
-	}
-	for (int i = 0; i < leftY.size(); i++) {
-		sumy += leftY.at(i);
-	}
-	if (sumx)
-
+void readInput() {
+	cin >> n >> k;
+	for (int i = 1; i < n; i++) cin >> X[i];
+	for (int i = 1; i < n; i++) cin >> Y[i];
 }
 
-void simul() {
+void solve() {
+	readInput();
+	dp[0][0] = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == 0 && j == 0) continue;
+			bool isFin = false;
 
-}
-
-int solve() {
-
+		}
+	}
 }
 
 int main(int argc, char** argv)
@@ -35,30 +34,9 @@ int main(int argc, char** argv)
 	cin >> T;
 	for (test_case = 0; test_case < T; test_case++)
 	{
-		Answer = 0;
-		/////////////////////////////////////////////////////////////////////////////////////////////
-		vector<int> X_card;
-		vector<int> Y_card;
-		cin >> n >> k;
-		for (int i = 0; i <= n; i++) {
-			int tmp;
-			cin >> tmp;
-			X_card.push_back(tmp);
-		}
-		for (int i = 0; i <= n; i++) {
-			int tmp;
-			cin >> tmp;
-			Y_card.push_back(tmp);
-		}
-		Answer = solve();
-
-
-		/////////////////////////////////////////////////////////////////////////////////////////////
-
-		// Print the answer to standard output(screen).
 		cout << "Case #" << test_case + 1 << endl;
-		cout << Answer << endl;
+		solve();  cout << endl;
 	}
 
-	return 0;//Your program should return 0 on normal termination.
+	return 0;
 }
