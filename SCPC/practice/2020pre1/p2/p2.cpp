06cc89c15037661fc6521c0b8a146444d47287d6
Seongwook Kang <gigaslender2@gachon.ac.kr>
2021-07-26 21:06:51 +0900

    Pass 2020pre1/p2

--- SCPC/practice/2020pre1/p2/p2.cpp
+++ SCPC/practice/2020pre1/p2/p2.cpp
@@ -7,46 +7,52 @@ using namespace std;
 int Answer;
 int n, k;
 int X[3001], Y[3001];
-int psX[3001], psY[3001];		//prefix sum of X and Y 
+int psX[3001], psY[3001];			//prefix sum of X and Y
+int ptnextX[3001], ptnextY[3001];
+int psdpX[3001][3001], psdpY[3001][3001];		//prefix sum of dp[X] and dp[Y]
 int dp[3001][3001];
 
 void readInput() {
 	cin >> n >> k;
-	for (int i = 1; i < n; i++) cin >> X[i];
-	for (int i = 1; i < n; i++) cin >> Y[i];
+	for (int i = 1; i <= n; i++) cin >> X[i];
+	for (int i = 1; i <= n; i++) cin >> Y[i];
 }
 
 void solve() {
 	readInput();
-	for (int i = 1; i < n; i++) psX[i] = psX[i - 1] + X[i];
-	for (int i = 1; i < n; i++) psY[i] = psY[i - 1] + Y[i];
+	for (int i = 1; i <= n; i++) psX[i] = psX[i - 1] + X[i];
+	for (int i = 1; i <= n; i++) psY[i] = psY[i - 1] + Y[i];
+	for (int i = 0; i <= n; i++) {
+		ptnextX[i] = lower_bound(psX, psX + i + 1, psX[i] - k) - psX;
+		ptnextY[i] = lower_bound(psY, psY + i + 1, psY[i] - k) - psY;
+	}
 
 	int ans1 = 0; int ans2 = 0;
-	dp[0][0] = 1;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+	dp[0][0] = psdpX[0][0] = psdpY[0][0] = 1;
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= n; j++) {
 			if (i == 0 && j == 0) continue;
 			bool isFin = false;
 			if (i > 0) {
-				int ptnextX = lower_bound(psX, psX + i + 1, psX[i] - k) - psX;
-				int caseX = i - ptnextX;
-				int sumX = 0;
-				for (int l = 1; l <= caseX; l++) sumX += dp[i - l][j];
+				int sumX = psdpX[i - 1][j];
+				if (ptnextX[i] > 0) sumX -= psdpX[ptnextX[i] - 1][j];
+				int caseX = i - ptnextX[i];
 				if (sumX < caseX) isFin = true;
 			}
 			if (j > 0) {
-				int ptnextY = lower_bound(psY, psY + j + 1, psY[j] - k) - psY;
-				int caseY = j - ptnextY;
-				int sumY = 0;
-				for (int l = 1; l <= caseY; l++) sumY += dp[i][j - l];
+				int sumY = psdpY[i][j - 1];
+				if (ptnextY[j] > 0) sumY -= psdpY[i][ptnextY[j] - 1];
+				int caseY = j - ptnextY[j];
 				if (sumY < caseY) isFin = true;
 			}
 			if (isFin) dp[i][j] = 1;
 			else dp[i][j] = 0;
+			psdpX[i][j] = (i > 0 ? psdpX[i - 1][j] : 0) + dp[i][j];
+			psdpY[i][j] = (j > 0 ? psdpY[i][j - 1] : 0) + dp[i][j];
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= n; j++) {
 			if (dp[i][j]) ans1++;
 			else ans2++;
 		}
