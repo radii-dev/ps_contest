41c5acfd20b6564f7117b33a4eb1dc6623d1cf3d
Seongwook Kang <gigaslender2@gachon.ac.kr>
2021-08-03 17:13:27 +0900

    Add p3.cpp

---
+++ SCPC/practice/2020pre1/p3/p3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int Answer;
+int N, k, m;
+
+void readInput() {
+	cin >> N >> k >> m;
+	for (int i = 0; i < k; i++) 
+	for (int i = 0; i < k; i++) 
+}
+
+void solve() {
+	readInput();
+
+	
+}
+
+int main(int argc, char** argv)
+{
+	int T, test_case;
+	freopen("p3sample_input.txt", "r", stdin);
+	
+	cin >> T;
+	for (test_case = 0; test_case < T; test_case++)
+	{
+		cout << "Case #" << test_case + 1 << endl;
+		solve();  cout << endl;
+	}
+
+	return 0;
+}
